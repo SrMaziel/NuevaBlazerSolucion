b78971a34f4b22b8af43b788da5514cc372edb7f
SrMaziel <mazielcalvo638@gmail.com>
2024-08-25 15:01:02 -0600

    Se agregaron carpetas en los services

---
+++ InformacionCrud.Cliente/Services/NacionalidadService.cs
@@ -0,0 +1,25 @@
+﻿using InformacionCrud.Shared;
+using System.Net.Http.Json;
+
+namespace InformacionCrud.Cliente.Services
+{
+    public class NacionalidadService : INacionalidadService
+    {
+        private readonly HttpClient _http;
+
+        public NacionalidadService(HttpClient http)
+        {
+            _http = http;
+        }
+
+        public async Task<List<NacionalidadDTO>> Lista()
+        {
+            var result = await _http.GetFromJsonAsync<ResponseAPI<List<NacionalidadDTO>>>("api/nacionalidad/Lista");
+
+            if (result!.EsCorrecto)
+                return result.Valor!;
+            else
+                throw new Exception(result.Mensaje);
+        }
+    }
+}
---
+++ InformacionCrud.Cliente/Services/TipodocumentoService.cs
@@ -0,0 +1,25 @@
+﻿using InformacionCrud.Shared;
+using System.Net.Http.Json;
+
+namespace InformacionCrud.Cliente.Services
+{
+    public class TipodocumentoService : ITipodocumentoService
+    {
+        private readonly HttpClient _http;
+
+        public TipodocumentoService(HttpClient http)
+        {
+            _http = http;
+        }
+
+        public async Task<List<TipodocumentosDTO>> Lista()
+        {
+            var result = await _http.GetFromJsonAsync<ResponseAPI<List<TipodocumentosDTO>>>("api/tipodocumento/Lista");
+
+            if (result!.EsCorrecto)
+                return result.Valor!;
+            else
+                throw new Exception(result.Mensaje);
+        }
+    }
+}
